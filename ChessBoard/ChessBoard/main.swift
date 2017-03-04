a8073e484cab5156301fbf9c5a970f2649381486
Brian <brawnquake@gmail.com>
2017-03-04 01:35:26 -0800

    added array class and moved array functions to it, commented code

--- ChessBoard/ChessBoard/main.swift
+++ ChessBoard/ChessBoard/main.swift
@@ -8,33 +8,11 @@
 
 import Foundation
 
-var globalSquareDimension = Int(arc4random_uniform(10) + 1)
-var globalRowCounter = 0
 
-func fillArray( array: [Int]) -> [Int]{
-    var chessBoardRow = array
-    for i in 0..<globalSquareDimension {
-        chessBoardRow.append(i + (globalRowCounter * globalSquareDimension))
-    }
-    return chessBoardRow
-}
-
-func printArray( array: [Int]) {
-    for element in array {
-        if element < 10{
-            print("\(element)  ", terminator:"")
-        }
-        else{
-            print("\(element) ", terminator:"")
-        }
-    }
-    print()
-}
-
-var newRow = [Int]()
-for row in 0..<globalSquareDimension {
-    var row = fillArray(array: newRow)
-    printArray(array: row)
-    globalRowCounter += 1
+var newRow = [Int]()//init an array
+for row in 0..<globalSquareDimension {//makes the array the length of N in an NxN square
+    var row = fillArray(array: newRow)//calls fill array
+    printArray(array: row)//calls print array
+    globalRowCounter += 1//increment row counter
 }
 
